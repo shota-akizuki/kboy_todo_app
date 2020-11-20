d55077a278f491a6547c8ad116626c0e46013995
Shota Akizuki <64979430+shota-akizuki@users.noreply.github.com>
2020-11-20 15:40:15 +0900

    4th Commit ・完了ボタンの実装。完了済みのtodoが削除できる。

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:todo_app/main_model.dart';
 
+import 'add/add_page.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
@@ -13,31 +15,68 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'TODOアプリ',
-      home: ChangeNotifierProvider<MainModel>(
-        create: (_) => MainModel()..getTodoListRealTime(),
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text('TODOアプリ'),
-          ),
-          body: Consumer<MainModel>(builder: (context, model, child) {
+        debugShowCheckedModeBanner: false, title: 'TODOアプリ', home: HomePage());
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider<MainModel>(
+      create: (_) => MainModel()..getTodoListRealTime(),
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('TODOアプリ'),
+          actions: [
+            Consumer<MainModel>(builder: (context, model, child) {
+              final isActive = model.checkShouldActiveCompleteButton();
+              return FlatButton(
+                child: Text(
+                  '完了',
+                  style: TextStyle(
+                    color:
+                        isActive ? Colors.white : Colors.white.withOpacity(0.5),
+                  ),
+                ),
+                onPressed: isActive
+                    ? () {
+                        model.deleteCheckedItems();
+                      }
+                    : null,
+              );
+            }),
+          ],
+        ),
+        body: Consumer<MainModel>(
+          builder: (context, model, child) {
             final todoList = model.todoList;
             return ListView(
               children: todoList
-                  .map(
-                    (todo) => ListTile(
-                      title: Text(todo.title),
-                    ),
-                  )
+                  .map((todo) => CheckboxListTile(
+                        title: Text(todo.title),
+                        value: todo.isDone,
+                        onChanged: (bool value) {
+                          todo.isDone = !todo.isDone;
+                          model.reload();
+                        },
+                      ))
                   .toList(),
             );
-          }),
-          floatingActionButton: FloatingActionButton(
-            onPressed: () {},
-            tooltip: 'Increment',
-            child: Icon(Icons.add),
-          ),
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          backgroundColor: Colors.orange,
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                fullscreenDialog: true,
+                builder: (context) => AddPage(),
+              ),
+            );
+          },
+          tooltip: 'Increment',
+          child: Icon(Icons.add),
         ),
       ),
     );
